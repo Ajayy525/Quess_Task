5b593b4366a21737f7e901687a13c1be79ccc214
Ajayy525 <109126840+Ajayy525@users.noreply.github.com>
2022-08-13 18:02:46 +0530

    Add files via upload

---
+++ Q32_CountUniqueElmnt.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Q32_CountUniqueElmnt {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = Sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements: ");
+        for(int i=0;i<n;++i){
+            arr[i] = Sc.nextInt();
+        }
+
+        int count = 1;
+
+        for(int i = 1; i < n; i++) {
+            int j;
+            for(j=0; j < i; j++) {
+                if(arr[i] == arr[j]) {
+                    break;
+                }
+            }
+            if(i == j){
+                count++;
+            }
+        }
+        System.out.println("The count is " + count);
+    }
+}
+
---
+++ Q33_SecondLargstElmnt.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Q33_SecondLargstElmnt {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = Sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter the values of array: ");
+        for(int i=0;i<n;++i){
+            arr[i] = Sc.nextInt();
+        }
+
+        int max = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;++i){
+            if(arr[i]>max){
+                second = max;
+                max = arr[i];
+            }
+            if(arr[i] < max && arr[i] > second){
+                second = arr[i];
+            }
+        }
+
+        System.out.print("The second largest no. is " + second);
+    }
+    }
+
