db27885faed17fa5a64ab24585578140f0679236
Ajayy525 <109126840+Ajayy525@users.noreply.github.com>
2022-08-14 09:29:38 +0530

    Delete SpringBoot/Books directory

--- SpringBoot/Books/Books/Books/src/main/java/com/Progard/BooksSql/Repository/BookRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Progard.BooksSql.Repository;
-
-import com.Progard.BooksSql.Models.Books;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepo extends JpaRepository<Books, Integer> {
-}
