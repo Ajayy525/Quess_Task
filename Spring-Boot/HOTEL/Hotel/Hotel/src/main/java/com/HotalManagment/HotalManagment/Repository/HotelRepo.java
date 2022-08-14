4b471523e14fa6f1d1978b4e92753636452c6180
Ajayy525 <109126840+Ajayy525@users.noreply.github.com>
2022-08-14 14:35:59 +0530

    Delete Spring-Boot/HOTEL/Hotel/Hotel directory

--- Spring-Boot/HOTEL/Hotel/Hotel/src/main/java/com/HotalManagment/HotalManagment/Repository/HotelRepo.java
+++
@@ -1,7 +0,0 @@
-package com.HotalManagment.HotalManagment.Repository;
-
-import com.HotalManagment.HotalManagment.Models.Hotels;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HotelRepo extends JpaRepository<Hotels, Integer> {
-}
